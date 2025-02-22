0c8c08c10f8c53a6e9b56d74a900bfb02c85e849
Ugur Eren <ugureren212@gmail.com>
2025-02-22 13:06:05 +0000

    forgot to commit this...

--- myproject/myapp/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views  # Import views from the app
-
-urlpatterns = [
-    path('', views.home, name='home'),  # Example view
-]
---
+++ serializer_example/snippets/serializers.py
@@ -0,0 +1,44 @@
+from rest_framework import serializers
+from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+
+# Serializer: Converts django models/python objects, JSON (for APIs) and other simple data types.
+# Deserializer: Converts JSON and other simple data types into django models/python objects.
+
+#Django REST Framework introduced ModelSerializer to simplify serialization by automatically mapping Django model fields.
+
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+
+
+
+
+# Before ModelSerializer, Django serializers were written manually using serializers.
+# Serializer, where you had to explicitly define each field and implement the create() and update() methods.
+
+# class SnippetSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     title = serializers.CharField(required=False, allow_blank=True, max_length=100)
+#     code = serializers.CharField(style={'base_template': 'textarea.html'})
+#     linenos = serializers.BooleanField(required=False)
+#     language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
+#     style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
+
+#     def create(self, validated_data):
+#         """
+#         Create and return a new `Snippet` instance, given the validated data.
+#         """
+#         return Snippet.objects.create(**validated_data)
+
+#     def update(self, instance, validated_data):
+#         """
+#         Update and return an existing `Snippet` instance, given the validated data.
+#         """
+#         instance.title = validated_data.get('title', instance.title)
+#         instance.code = validated_data.get('code', instance.code)
+#         instance.linenos = validated_data.get('linenos', instance.linenos)
+#         instance.language = validated_data.get('language', instance.language)
+#         instance.style = validated_data.get('style', instance.style)
+#         instance.save()
+#         return instance
